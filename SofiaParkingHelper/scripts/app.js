e25313f072915e12f318628ec25272fa97531f1c
Kiril Tabakov <kirotab@gmail.com>
2013-10-10 14:33:06 -0500

    Changes made by Kiril Tabakov (kirotab@gmail.com).

--- SofiaParkingHelper/scripts/app.js
+++ SofiaParkingHelper/scripts/app.js
@@ -1,21 +1,11 @@
-(function (global) {
-    var mobileSkin = "",
-        app = global.app = global.app || {};
+var app = app || {};
 
-    document.addEventListener("deviceready", function () {
-        app.application = new kendo.mobile.Application(document.body, { layout: "tabstrip-layout" });
-    }, false);
-
-    app.changeSkin = function (e) {
-        if (e.sender.element.text() === "Flat") {
-            e.sender.element.text("Native");
-            mobileSkin = "flat";
-        }
-        else {
-            e.sender.element.text("Flat");
-            mobileSkin = "";
-        }
-
-        app.application.skin(mobileSkin);
-    };
-})(window);
+(function() {
+    
+    document.addEventListener("deviceready", function() {
+        //app.servicesBaseUrl = "http://localhost:62354/api/";
+        app.servicesBaseUrl = "http://the-places.apphb.com/api/";
+        
+        var kendoApp = new kendo.mobile.Application(document.body);
+    });    
+}());
